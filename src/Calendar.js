fa1931d1b66ab38b0fc43906d669c8e4d0482015
Chris Caragianis <chriscaragianis@gmail.com>
2017-09-11 21:58:03 -0400

    pulls events from api

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import _ from 'lodash';
 
 BigCalendar.setLocalizer(
   BigCalendar.momentLocalizer(moment)
 );
 
+
 class Calendar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    }
+  }
+  componentDidMount() {
+    const request = new Request('http://localhost:4000/api/v1/events', {
+      method: 'GET',
+    });
+    const events = fetch(request)
+      .then((res) => { return res.json(); })
+      .then((json) => {
+        const ev = _.map(json, (x) => {
+          x.start = new Date(x.start);
+          x.end = new Date(x.end);
+        })
+        this.setState({
+          events: json,
+        })
+      })
+  }
+
   render() {
     return(
       <div className="calendar">
         <BigCalendar
-          events={[]}
-          startAccessor='startDate'
-          endAccessor='endDate'
+          events={this.state.events}
+          startAccessor='start'
+          endAccessor='end'
         />
     </div>
     );
