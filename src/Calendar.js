112237527e76477dc3914d8a89c03310ba5d5f1b
Chris Caragianis <chriscaragianis@gmail.com>
2017-10-24 08:45:09 -0400

    add fixture event and some plumbing

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BigCalendar from 'react-big-calendar';
+import ReactModal from 'react-modal';
 import moment from 'moment';
 import _ from 'lodash';
 
@@ -7,13 +8,32 @@ BigCalendar.setLocalizer(
   BigCalendar.momentLocalizer(moment)
 );
 
+const eventsFixture = [
+  {
+    startTime: moment('2017-11-08 09'),
+    endTime: moment('2017-11-08 11'),
+    title: 'title',
+    location: 'location',
+  }
+];
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class Calendar extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      events: [],
+      events: eventsFixture,
     }
   }
   componentDidMount() {
@@ -24,8 +44,8 @@ class Calendar extends React.Component {
       .then((res) => { return res.json(); })
       .then((json) => {
         const ev = _.map(json, (x) => {
-          x.start = new Date(x.start);
-          x.end = new Date(x.end);
+          x.startTime = new Date(x.startTime);
+          x.endTime = new Date(x.endTime);
         })
         this.setState({
           events: json,
@@ -37,9 +57,15 @@ class Calendar extends React.Component {
     return(
       <div className="calendar">
         <BigCalendar
+          selectable
           events={this.state.events}
-          startAccessor='start'
-          endAccessor='end'
+          startAccessor='startTime'
+          endAccessor='endTime'
+          onSelectEvent={(e) => { console.log(e); alert('selected!'); }}
+        />
+        <ReactModal
+          isOpen={this.state.modalOpen}
+          styles={customStyles}
         />
     </div>
     );
