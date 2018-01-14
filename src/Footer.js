3f8d15fe5c89b9e4c3fd0611b420446a78f68ff3
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-14 12:07:53 -0500

    social icons added

--- src/Footer.js
+++ src/Footer.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer-container">
-    Footer
-    <ul>
-      <li>Social buttons</li>
-      <li>small menu</li>
-      <li>contact info</li>
-      <li>join dsa link</li>
-    </ul>
+    <div className="social-icons">
+      <ul>
+        <li>
+          <img src="facebook.svg" alt="facebook link" />
+        </li>
+        <li>
+          <img src="twitter.svg" alt="twitter link" />
+        </li>
+        <li>
+          <img src="gmail.svg" alt="gmail link" />
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
