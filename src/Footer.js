f417050dab115c096061f3a39b4072f2aa3eb52d
Chris Caragianis <chriscaragianis@gmail.com@>
2018-01-14 08:53:43 -0500

    todos for footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,12 @@ import React from 'react';
 const Footer = () => (
   <div className="footer-container">
     Footer
+    <ul>
+      <li>Social buttons</li>
+      <li>small menu</li>
+      <li>contact info</li>
+      <li>join dsa link</li>
+    </ul>
   </div>
 );
 
