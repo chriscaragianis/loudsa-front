403fe5e43bae410a69b67588fe55dc0c69e94bcb
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-14 12:33:29 -0500

    add footer columns

--- src/Footer.js
+++ src/Footer.js
@@ -2,18 +2,26 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer-container">
-    <div className="social-icons">
-      <ul>
-        <li>
-          <img src="facebook.svg" alt="facebook link" />
-        </li>
-        <li>
-          <img src="twitter.svg" alt="twitter link" />
-        </li>
-        <li>
-          <img src="gmail.svg" alt="gmail link" />
-        </li>
-      </ul>
+    <div className="contact-column footer-column">
+      <div className="social-icons">
+        <ul>
+          <li>
+            <img src="facebook.svg" alt="facebook link" />
+          </li>
+          <li>
+            <img src="twitter.svg" alt="twitter link" />
+          </li>
+          <li>
+            <img src="gmail.svg" alt="gmail link" />
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="menu-column footer-column">
+      Menu
+    </div>
+    <div className="message-column footer-column">
+      Message
     </div>
   </div>
 );
