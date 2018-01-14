e29d1a19a7e530f338213d0ef5f358455fa9637c
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-14 18:30:09 -0500

    footer layout looks ok

--- src/Footer.js
+++ src/Footer.js
@@ -1,27 +1,36 @@
 import React from 'react';
+import Menu from './Menu.js';
 
 const Footer = () => (
   <div className="footer-container">
     <div className="contact-column footer-column">
       <div className="social-icons">
-        <ul>
-          <li>
-            <img src="facebook.svg" alt="facebook link" />
-          </li>
-          <li>
-            <img src="twitter.svg" alt="twitter link" />
-          </li>
-          <li>
-            <img src="gmail.svg" alt="gmail link" />
-          </li>
-        </ul>
+        <img src="facebook.svg" alt="facebook link" />
+        <img src="twitter.svg" alt="twitter link" />
+        <img src="gmail.svg" alt="gmail link" />
+      </div>
+      <div className="footer-address">
+        Louisville DSA<br/>
+        123 Debs Dr <br/>
+        Louisville, KY 40217 <br/>
+        <br/>
+        (502) 555-1917
       </div>
     </div>
     <div className="menu-column footer-column">
-      Menu
+      <Menu className="footer-menu" />
     </div>
     <div className="message-column footer-column">
-      Message
+      <p>
+        <a href="https://dsausa.org">Democratic Socialists</a> believe that both
+        the economy and society should be run democratically to meet human needs,
+        not to make profits for a few. We are a political and activist organization,
+        not a party; through campus and community-based chapters DSA members use a
+        variety of tactics, from legislative to direct action, to fight for reforms
+        that empower working people.
+      </p>
+      <br />
+      <a href="https://act.dsausa.org/donate/membership/">Join DSA!</a>
     </div>
   </div>
 );
--- src/Menu.js
+++ src/Menu.js
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 import MenuItem from './MenuItem';
 import _ from 'lodash';
 
-const Menu = () => (
-  <div className="menu">
-    <Link className="menu-item" to="/About">About</Link>
-    <Link className="menu-item" to="/News">News</Link>
-    <Link className="menu-item" to="/Events">Events</Link>
-    <Link className="menu-item" to="/Socialism">Socialism</Link>
-    <Link className="menu-item" to="/Info">Info</Link>
-  </div>
-);
+const Menu = (props) => {
+  const { className } = props;
+  return (
+    <div className={ className || "menu" }>
+      <Link className="menu-item" to="/About">About</Link>
+      <Link className="menu-item" to="/News">News</Link>
+      <Link className="menu-item" to="/Events">Events</Link>
+      <Link className="menu-item" to="/Socialism">Socialism</Link>
+      <Link className="menu-item" to="/Info">Info</Link>
+    </div>
+  );
+};
 
 export default Menu;
