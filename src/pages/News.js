b1c00ab50e0dc99dbf35029f5fc9df97fbc515e6
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-15 19:30:01 -0500

    news page laid out and sorted with sample data

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import map from 'lodash/map';
+import sortBy from 'lodash/sortBy';
+import reverse from 'lodash/reverse';
+import moment from 'moment';
+import news from '../sample_news';
 
-class News extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="news-page">
-        <ul>
-          <li>News</li>
-          <li>items</li>
-          <li>here</li>
-        </ul>
-      </div>
-    );
-  }
+const makeNewsBody = (n) => {
+  return (
+    <p>{n.body}</p>
+  );
 }
+const makeNews = (newses) => {
+  return map(reverse(sortBy(newses, (n) => n.date)), (n) => (
+    <li className="news-item">
+      <p>{moment(n.date).format('dddd MMMM Do YYYY')}</p>
+      <h1>{n.title}</h1>
+      {makeNewsBody(n)}
+    </li>
+  ));
+};
+
+const News = () => (
+  <div className="news-page">
+    <ul>
+      {makeNews(news)}
+    </ul>
+  </div>
+);
 
 export default News;
