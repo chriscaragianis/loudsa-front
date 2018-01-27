734a83c61e1b151a4de0bbc9b0d75ee771cc7cf2
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-27 10:32:22 -0500

    WIP

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Info = () => (
   <div className="info-page">
-    <ul>
-      <li>Bylaws</li>
+     <ul>
+      <li>
+        Bylaws of the Louisville KY Local of the Democratic Socialists of America <a href="https://s3.us-east-2.amazonaws.com/loudsa-public/bylaws.pdf">pdf</a>
+      </li>
       <li>New member guide</li>
       <li>Org structure</li>
       <li>DSA FAQ</li>
