98953ecc0c4a91ba5d9034dd9d3425ad4836c14c
Chris Caragianis <chriscaragianis@gmail.com>
2018-01-28 22:37:52 -0500

    fix margin issue

--- src/Image.js
+++ src/Image.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Image = (props) => (
-  <div className="image-container">
-    <img alt={props.alt} src={props.src} />
+  <div>
+    <div className="image-container">
+      <img alt={props.alt} src={props.src} />
+    </div>
     <div className="image-caption">
       {props.caption}
     </div>
