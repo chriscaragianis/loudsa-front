1bd0b78dee6704dbcce3ff250080a65b181b6f7b
Chris Caragianis <chriscaragianis@gmail.com>
2018-02-03 16:02:57 -0500

    Copy for socialism page

--- src/pages/Socialism.js
+++ src/pages/Socialism.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Image from '../Image';
-import Lorem from '../lorem';
 
 const Socialism = () => (
   <div className="page about-page">
     <div className="copy about-copy">
-      <h1>Socialism</h1>
-      { Lorem }
+      <h1>What is Socialism?</h1>
+      <p>
+Socialism is an economic and political philosophy first outlined by Karl Marx as an evolution from capitalism. To understand socialism, it is useful to first describe capitalism. To Marx, capitalism is “private ownership of the means of production.” What that means in practice is that you have a small group of people who own factories or companies and that these people hire other people to work the factories and pay them wages to create some product or to fulfill roles in the company to perform some service. In order for this small group of people who own the factories and companies to make money, or “profit,” they sell the product or service for more than the cost of materials and wages they pay their workers. This means that workers’ wages do not reflect the full value of their labor, because the owner’s profit is taken from them. In fact, wages that the owners pay their workers directly reduce their profit, and so owners minimize the wages they pay their workers as much as they can.
+</p>
+  <p>
+Beyond underpayment, a lot of other problems flow from this arrangement, which Marx outlined 150 years ago. We don’t need to go into all of them right now, it’s enough to say that a society depends on products and services created and performed by its members in order to function--from food, clothing, and housing, to medical care, child care, sanitation, and maintenance of roads, buildings, and machines. When a very few people control the means by which those goods and services are produced and performed, those few can exert a great deal of control over society. The vast majority of people must then work for those few, and follow the rules set by those few, or else they will suffer without food, shelter, or medical care. It is hard to see how democracy flourishes when most people have to beg a few people with a lot of power for their basic needs. We call this arrangement, where few people control the lives and fortunes of the majority of people, “oppression.”
+  </p>
+  <p>
+With that understanding, the most basic and true definition of socialism is “worker ownership of the means of production, distribution, and exchange.” The idea is that there shouldn’t be a very few people who own the means to make society function for all of its members. Instead, those means should be shared by all, and used to benefit all, with the control of them determined democratically. However, what that looks like in practice has been the subject of a great deal of debate over the course of the 150 years since the idea was initially formed. Furthermore, whenever it has been attempted, socialist governments have been set upon by these relatively few, powerful people (and the capitalist governments they control) to undermine them by funding coups from within, outright attacking them, or at the very least, placing severe restrictions on trade with them, depriving them of fair markets and beggaring their people. These practices have most often led to authoritarian governments, with little democratic input from their people.
+  </p>
+  <p>
+The Democratic Socialists of America are committed to a truly democratic society that reflects the values and meets the needs of people who have been devalued by capitalism, in and out of the workplace. Socialism, at its core, is a system that takes the power from the few (the capitalist class of corporate officers and landowners), and raises up the power of the many (the workers who produce value and contribute to our society).
+</p>
+  <p>
+In DSA’s view, socialism is a tool to flatten the hierarchy of the world we live in, where white men with moneyed families make all the decisions, and the working class produces value that they never benefit from. By freeing the working class from the oppression of capitalism, socialism is the weapon that the working class can turn against racism, patriarchy, cisheteronormativity, and all other forms of oppression that divide working people and keep the wealthy on top of their perch, while also keeping the destruction of our environment in check through economic planning and regulation of the use of our natural resources.
+</p>
     </div>
     <div className="image-strip">
       <Image src="http://lorempixel.com/200/200" caption="A caption for the picture" />
