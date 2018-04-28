d8f1559cd39621b21420dfb807a0464e5768a09e
acrobertson <alec.robertson08@gmail.com>
2018-04-27 20:26:33 -0400

    added real events and news
    Swapped out the sample data with (soon to be outdated) real stuff. Will remove "sample" from the names at some point. And hopefully make addding content easier. Eventually.

--- src/sample_news.js
+++ src/sample_news.js
@@ -1,34 +1,17 @@
 const news = [
   {
-    date: new Date(2018, 0, 1, 0, 0, 0, 0),
-    title: 'news item with a picture here',
-    body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    image: 'http://lorempixel.com/200/200',
-    alt: 'Image to accompany news item',
+    date: new Date(2018, 3, 2, 0, 0, 0, 0),
+    title: 'Kentucky Teachers Protest the Pension Reform Bill',
+    body: 'In response to the Kentucky Legislature reforming the public pension plan as an addendum to a sewage bill, thousands of Kentucky teachers protested the bill in Frankfort. After the bill was passed Thursday night, enough teachers called in sick on Friday, March 31 to cancel school in over 20 counties in Kentucky. On Monday, April 2, all 120 counties in Kentucky cancelled school between strikes and spring break as the teachers made their way to Frankfort. Louisville DSA members were there in solidarity with the teachers, providing donated water, snacks, and gas cards to help the teachers fight for their promised pensions.',
+    image: 'teacher_rally.jpg',
+    alt: 'DSA members hold #120Strong banner at teacher rally',
   },
   {
-    date: new Date(2016, 0, 1, 0, 0, 0, 0),
-    title: 'news item with a picture here',
-    body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    image: 'http://lorempixel.com/400/200',
-    alt: 'Image to accompany news item',
-  },
-  {
-    date: new Date(2016, 0, 1, 0, 0, 0, 0),
-    title: 'news item with a picture here',
-    body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    image: 'http://lorempixel.com/200/400',
-    alt: 'Image to accompany news item',
-  },
-  {
-    date: new Date(2017, 0, 14, 0, 0, 0, 0),
-    title: 'old news item here',
-    body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-  },
-  {
-    date: new Date(2018, 0, 14, 0, 0, 0, 0),
-    title: 'Fresh news item here',
-    body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    date: new Date(2018, 3, 22, 0, 0, 0, 0),
+    title: 'DSA National Medicare for All Weekend of Action',
+    body: 'As part of DSA’s Medicare for All Weekend of Action, DSA Louisville members canvassed Kentucky’s 35th district in favor of single payer healthcare for DSA member Richard Becker’s campaign for state representative. Our canvassers discussed the necessity of single payer healthcare with the constituents of district 35 and explained why Richard is the best candidate to achieve our healthcare goals. Our local Medicare for All campaign is just getting started, and we will have many more opportunities to get involved as the year goes on!',
+    image: 'becker_canvas.jpg',
+    alt: 'DSA members posing with Richard Becker campaign materials',
   }
 ];
 
